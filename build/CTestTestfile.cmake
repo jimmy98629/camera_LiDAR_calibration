5104a114167ec7300fc246e7d20cb00a6ec4ed7d
Song Young Bo <72201661+gomduribo@users.noreply.github.com>
2022-11-02 19:06:40 +0900

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jimmy98629/calib_ws/src
-# Build directory: /home/jimmy98629/calib_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("lidar_camera_calibration")
