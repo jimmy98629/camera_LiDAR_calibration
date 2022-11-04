8a2d3f1788502374d79e494adc6dfbaa4104bd4b
SongYoungBo <syb6245@naver.com>
2022-11-04 14:07:47 +0900

    calibrate 함수, extract 3d 함수 수정

--- main.py
+++ main.py
@@ -77,12 +77,12 @@ def extract_3d_points(path):
 
     # convert Open3D.o3d.geometry.PointCloud to numpy array
     # xyz_load = np.asarray(pcd_load.points)
-    inrange = np.where((lidar_data[:, 0] > -20) &
+    inrange = np.where((lidar_data[:, 0] > 0) &
                        (lidar_data[:, 0] < 20) &
                        (np.abs(lidar_data[:, 1]) < 20) &
                        (lidar_data[:, 2] < 20))
     lidar_data = lidar_data[inrange[0]]
-    # print(lidar_data.shape)
+    print(lidar_data.shape)
     # print(inrange)
 
     cmap = matplotlib.cm.get_cmap('hsv')
@@ -93,12 +93,12 @@ def extract_3d_points(path):
     ax.set_title('Select 3D LiDAR Points')
     ax.set_axis_off()
     ax.set_facecolor((0, 0, 0))
-    ax.scatter(lidar_data[:, 0], lidar_data[:, 1], lidar_data[:, 2], c=colors, s=2, picker=5)
+    ax.scatter(lidar_data[:, 0], lidar_data[:, 1], lidar_data[:, 2], c=colors, s=0.07, picker=5)
 
     # Equalize display aspect ratio for all axes
     max_range = (np.array([lidar_data[:, 0].max() - lidar_data[:, 0].min(), 
         lidar_data[:, 1].max() - lidar_data[:, 1].min(),
-        lidar_data[:, 2].max() - lidar_data[:, 2].min()]).max() / 2.0)
+        lidar_data[:, 2].max() - lidar_data[:, 2].min()]).max() / 8.0)
     mid_x = (lidar_data[:, 0].max() + lidar_data[:, 0].min()) * 0.5
     mid_y = (lidar_data[:, 1].max() + lidar_data[:, 1].min()) * 0.5
     mid_z = (lidar_data[:, 2].max() + lidar_data[:, 2].min()) * 0.5
@@ -142,16 +142,19 @@ def calibrate(points_2d, points_3d):
 
                     [0, 0, 1]])
     
-    dist_coeff = np.array([[0,0,0,0,0]])
+    dist_coeff = np.array([[0.0,0.0,0.0,0.0,0.0]])
     points_2d = np.array(points_2d)
     points_3d = np.array(points_3d)
     print(points_2d.shape)
     print(points_3d.shape)
-    success, rotation_matrix, translation_matrix, inlier = cv2.solvePnPRansac(points_2d,
-    points_3d, intrinsic_cam_matrix, dist_coeff, flags=cv2.SOLVEPNP_ITERATIVE)
-    
-    rotation_matrix = cv2.Rodrigues(rotation_matrix)[0]
-    return rotation_matrix
+    success, rotation_matrix, translation_matrix, inlier = cv2.solvePnPRansac(points_3d,
+    points_2d, intrinsic_cam_matrix, dist_coeff, flags=cv2.SOLVEPNP_ITERATIVE)
+    print("rotation matrix",rotation_matrix)
+    print("translation matrix", translation_matrix)
+
+    rotation_matrix = cv2.Rodrigues(rotation_matrix)
+    # print("rotation matrix2: ",rotation_matrix[0])
+    return rotation_matrix[0]
 
 def projection():
     
@@ -159,13 +162,14 @@ def projection():
 
 if __name__ == '__main__':
     # path_lidar = "camera_LiDAR_calibration/lidar/0.npy"
-    path_lidar = "camera_LiDAR_calibration/calib_checker/lidar/00/lidar_227.pcd"
+    path_lidar = "camera_LiDAR_calibration/calib_checker/lidar/01/lidar_0.pcd"
     path_image = "camera_LiDAR_calibration/camera/37.png"
     corner = extract_corner_points(path_image)
     print("선택된 2D 코너: ",corner)
     print("2D type: ",np.array(corner))
     var = extract_3d_points(path_lidar)
     print("선택된 3D 좌표: ",var)
+    # print("선택된 3d 크기: ", var.s)
     rot = calibrate(corner, var)
     print("rotation matrix: ", rot)
     
